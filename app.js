d4fccde6ede79ca4e4e2cfb3b93d0b8d519136e0
Tomas Vykoukal <vykoukal.tom@gmail.com>
2024-06-02 21:19:35 +0200

    WIP

--- app.js
+++ app.js
@@ -1,67 +1,74 @@
-const numberButtons = document.querySelectorAll('[data-number]')
-const operatorDivide = document.querySelector('[data-divide]')
-const operatorEquals = document.querySelector('[data-equals]')
-const operatorPlus = document.querySelector('[data-plus]')
-const operatorMinus = document.querySelector('[data-minus]')
-const operatorMultiplication = document.querySelector('[data-multiplication]')
-const operatorPeriod = document.querySelector('[data-period]')
-const operatorDelete = document.querySelector('[data-delete]')
-const operatorAC = document.querySelector('[data-AC]')
-const operatorAll = document.querySelectorAll('[data-operator]')
+const calculator = document.getElementById('calculator')
+const output = document.querySelector(".output")
 
 let operation = []
 
+const operators = ["/", "*", "+", "-"]
+
 //operation buttons 
-function operationFunction(event){
-    operation.push(event.target.innerText)
-    document.querySelector(".output").innerHTML = operation.join('');
+const operationFunction = (event) => {
+    console.log("ss");
+    if (event.target.innerText !== operation[operation.length - 1]) {
+        operation.push(event.target.innerText)
+    }
+    output.innerHTML = operation.join('')
 }
 
 //divide button 
-function divideFunction(){
+function divideFunction() {
     operation.push(' / ')
-    document.querySelector(".output").innerHTML = operation.join('');
+    output.innerHTML = operation.join('');
 }
 
 //equals button  
-function equalsFunction(){
+function equalsFunction() {
     let result = eval(operation.join(''))
-    document.querySelector(".output").innerHTML = result;
+    output.innerHTML = result;
     operation = [result]
 }
 
 //all clear button 
-function clearFunction(){
+function clearFunction() {
     operation = []
-    document.querySelector(".output").innerHTML = operation.join('');
+    output.innerHTML = operation.join('');
 }
 
 //delete button
-function deleteFunction(){
-    operation.pop(); 
-    document.querySelector(".output").innerHTML = operation.join('');
+function deleteFunction() {
+    operation.pop();
+    output.innerHTML = operation.join('');
 }
 
 //operators 
-function removeDuplicateOperators(){
+function removeDuplicateOperators() {
     console.log(operation)
-    if (operation[operation.length - 1] === operation[operation.length - 2]){
+    if (operation[operation.length - 1] === operation[operation.length - 2]) {
         operation.pop()
-    } 
-    document.querySelector(".output").innerHTML = operation.join('');
+    }
+    output.innerHTML = operation.join('');
 }
 
-operatorPlus.addEventListener('click', operationFunction)
-operatorMinus.addEventListener('click', operationFunction)
-operatorMultiplication.addEventListener('click', operationFunction)
-numberButtons.forEach(function(button){
-    button.addEventListener('click', operationFunction);
-});
-operatorPeriod.addEventListener('click', operationFunction)
-operatorDivide.addEventListener('click', divideFunction)
-operatorEquals.addEventListener('click', equalsFunction)
-operatorAC.addEventListener('click', clearFunction)
-operatorDelete.addEventListener('click', deleteFunction)
-operatorAll.forEach(operator => {
-    operator.addEventListener('click', removeDuplicateOperators);
-});
+calculator.addEventListener('click', (e) => {
+    console.log('>>> e', e)
+
+    if (e.target.attributes.data?.value && operators.includes(e.target.attributes.data.value)) {
+        console.log('>>> je to operator')
+    }
+})
+
+
+// na vysvetleni
+// calculator.addEventListener('click', (e) => operationFunction(e))
+// calculator.addEventListener('click', operationFunction)
+
+const func = (value, funcArg) => {
+    funcArg(value)
+}
+
+
+const func2 = (value) => {
+    console.log('>>> value', value)
+}
+
+func("click", func2)
+func("click", (e) => func2(e))
